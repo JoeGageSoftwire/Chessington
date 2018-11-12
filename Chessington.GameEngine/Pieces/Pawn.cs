db2573e2e571515e326278c30c42ebf8c984916a
Joseph Gage <Joseph.Gage@softwire.com>
2018-11-12 15:25:25 +0000

    Made all pawns move more sensibly

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -13,24 +14,39 @@ namespace Chessington.GameEngine.Pieces
         {
             var moves = new List<Square>();
             { };
+
+            var row = board.FindPiece(this).Row;
+            var col = board.FindPiece(this).Col;
             
-            if (HasMoved)
+            if (!HasMoved)
             {
                 if (Player == Player.White)
                 {
-                    moves.Add(Square.At(5, 2));
+                    moves.Add(Square.At(row - 1, col));
+                    moves.Add(Square.At(row - 2, col));
                 }
                 else
                 {
-                    moves.Add(Square.At(7, 2));
+                    moves.Add(Square.At(row + 1, col));
+                    moves.Add(Square.At(row + 2, col));
                 }
             }
             else
             {
-                moves.Add(Square.At(2, 0));
-                moves.Add(Square.At(6, 0));
-                moves.Add(Square.At(3, 3));
-                moves.Add(Square.At(5, 5));
+                if (Player == Player.White)
+                {
+                    if (row != 0)
+                    {
+                        moves.Add(Square.At(row - 1, col));
+                    }
+                }
+                else
+                {
+                    if (row != 8)
+                    {
+                        moves.Add(Square.At(row + 1, col));
+                    }
+                }
             }
 
             return moves;
