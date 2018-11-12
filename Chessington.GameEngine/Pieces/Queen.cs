7ae2705551cc21859244f9868af4fccd92c1b806
Joseph Gage <Joseph.Gage@softwire.com>
2018-11-12 16:16:50 +0000

    Made queens move

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +11,38 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var moves = new List<Square>();
+            var row = board.FindPiece(this).Row;
+            var col = board.FindPiece(this).Col;
+
+            for (int i = 0; i <= 7; i++)
+            {
+                if (i != col)
+                {
+                    moves.Add(Square.At(row, i));
+                }
+
+                if (i != row)
+                {
+                    moves.Add(Square.At(i, col));
+                }
+
+                var diff = Math.Abs(row - i);
+                if (diff != 0)
+                {
+                    if (col + diff <= 7)
+                    {
+                        moves.Add(Square.At(i, col + diff));
+                    }
+
+                    if (col - diff >= 0)
+                    {
+                        moves.Add(Square.At(i, col - diff));
+                    }
+                }
+            }
+
+            return moves;
         }
     }
 }
